dcc75e5d5448f62bcec93b1480965aba12671c18
Akmal Khan <akmalnawabi007@gmail.com>
2025-06-30 12:43:31 +0330

    final

--- src/assets/images/services_data.js
+++ src/assets/images/services_data.js
@@ -7,27 +7,27 @@ const Services_Data = [
     {
         s_no:"02",
         s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Using Adope photoshop for designing..."
     },
     {
         s_no:"03",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Akmal Nawabi Facebook and Instagram accounts"
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"App development",
+        s_desc:"Application development using React native..."
     },
     {
         s_no:"05",
         s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"A little familier with Amazon and Google Ads..."
     },
     {
         s_no:"06",
         s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Making documentaion for projects..."
     },
 ]
  
